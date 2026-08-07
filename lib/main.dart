d2b8f34e929a0a364c8f4e4ae4ef022f0c4a8d4a
Kalpeshk <Kalpeshk@letshego.com>
2026-08-07 12:29:46 +0530

    Flutter Analyze

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'dart:math';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int age = 20;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'CI/CD Demo',
